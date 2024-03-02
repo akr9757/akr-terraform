5446b5f785e8648d293e6668e31d54f4d9b50a21
Lenovo <anilkumareddy9757@gmail.com>
2024-03-02 14:43:21 +0530

    printing heading

--- main.tf
+++ main.tf
@@ -11,6 +11,6 @@ resource "aws_route53_record" "www" {
   zone_id = "Z097978826RFVR2P0Q5DM"
   name    = "frontend-dev.akrdevopsb72.online"
   type    = "A"
-  ttl     = 300
+  ttl     = 30
   records = [aws_instance.web.private_ip]
 }
