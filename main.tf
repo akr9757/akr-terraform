ca1668e48d10d9567411bc2cbf4508bd17fc7732
Lenovo <anilkumareddy9757@gmail.com>
2024-03-02 15:18:55 +0530

    printing heading

--- main.tf
+++ main.tf
@@ -1,69 +1,69 @@
 resource "aws_instance" "instance" {
-  count = length(var.components)
+  for_each = var.components
   ami           = data.aws_ami.ami.id
   instance_type = "t3.micro"
   vpc_security_group_ids = [data.aws_security_group.sg.id]
 
   tags = {
-    Name = "${var.components[count.index]}"
+    Name = "each.value[name]"
   }
 }
 
 
 resource "aws_route53_record" "www" {
-  count = length(var.components)
+  for_each = var.components
   zone_id = "Z097978826RFVR2P0Q5DM"
-  name    = "${var.components[count.index]}-dev.akrdevopsb72.online"
+  name    = "each.value[name]-dev.akrdevopsb72.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.instance.private_ip]
 }
 
 variable "components" {
-  default = [
+  default = {
     frontend = {
-      name = "frontend"
+      name          = "frontend"
       instance_type = "t3.micro"
     }
     catalogue = {
-      name = "catalogue"
+      name          = "catalogue"
       instance_type = "t3.micro"
     }
     cart = {
-      name = "cart"
+      name          = "cart"
       instance_type = "t3.micro"
     }
     user = {
-      name = "user"
+      name          = "user"
       instance_type = "t3.micro"
     }
     shipping = {
-      name = "shipping"
+      name          = "shipping"
       instance_type = "t3.micro"
     }
     payment = {
-      name = "payment"
+      name          = "payment"
       instance_type = "t3.micro"
     }
     dispatch = {
-      name = "dispatch"
+      name          = "dispatch"
       instance_type = "t3.micro"
     }
     mongodb = {
-      name = "mongodb"
+      name          = "mongodb"
       instance_type = "t3.micro"
     }
     redis = {
-      name = "redis"
+      name          = "redis"
       instance_type = "t3.micro"
     }
     mysql = {
-      name = "mysql"
+      name          = "mysql"
       instance_type = "t3.micro"
     }
     rabbitmq = {
       name          = "rabbitmq"
       instance_type = "t3.micro"
     }
-  ]
+  }
 }
