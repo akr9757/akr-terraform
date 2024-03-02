bf08190d564ba1d0d13e4882a20e5bd454b18c0e
Lenovo <anilkumareddy9757@gmail.com>
2024-03-02 15:27:10 +0530

    printing heading

--- main.tf
+++ main.tf
@@ -10,14 +10,14 @@ resource "aws_instance" "instance" {
 }
 
 
-#resource "aws_route53_record" "www" {
-#  for_each = var.components
-#  zone_id = "Z097978826RFVR2P0Q5DM"
-#  name    = "each.value[name]-dev.akrdevopsb72.online"
-#  type    = "A"
-#  ttl     = 30
-#  records = [aws_instance.instance.instanceprivate_ip]
-#}
+resource "aws_route53_record" "www" {
+  for_each = var.components
+  zone_id = "Z097978826RFVR2P0Q5DM"
+  name    = "each.value[name]-dev.akrdevopsb72.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instance[each.value[name]].private_ip]
+}
 
 variable "components" {
   default = {
