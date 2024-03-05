d269a2c2165a37a0d7f42d2478bcc876d84515ed
Lenovo <anilkumareddy9757@gmail.com>
2024-03-05 15:57:54 +0530

    printing heading

--- servers.tf
+++ servers.tf
@@ -3,6 +3,7 @@ module "servers" {
 
   source = "./module"
   component_name = each.value["name"]
+  instance_type = each.value["instance_type"]
   password = lookup(each.value, "password", "null")
   env = var.env
 
